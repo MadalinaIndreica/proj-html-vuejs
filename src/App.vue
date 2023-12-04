f69e7b5df05e94472470344ed32e6f7dc75471a1
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2023-12-04 16:14:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+
+export default  {
+  data (){
+    return {
+
+    }
+  },
+  components : { AppHeader, AppFooter }
+}
+
+</script>
+
+<template>
+  <AppHeader>
+
+  </AppHeader>
+  <AppFooter></AppFooter>
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <header>
+       
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
